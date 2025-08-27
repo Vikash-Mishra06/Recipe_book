e0f6117d445686b44817e6ec084579cf5298095a
Vikash Mishra <mishravikash0612@gmail.com>
2025-08-27 23:42:02 +0530

    Third commit

--- recipe_book/src/pages/Home.jsx
+++ recipe_book/src/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import axios from '../utils/axios'
 
 const Home = () => {
+  const getproduct = async () => {
+    try{
+      const response = await axios.get("/products")
+      console.log(response.data)
+    } catch(error) {
+      console.log(error)
+    }
+  }
   return (
-    <div>Home</div>
+    <div>
+      <button onClick={getproduct}>Get Products</button>
+    </div>
   )
 }
 
